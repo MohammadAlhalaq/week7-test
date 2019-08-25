5147eb1be2d811e2d3d118ccfb39a2839dfb75b1
mohammadalhallaq <mohammadalhallaq@pop-os.localdomain>
2019-08-25 23:47:25 +0300

    create database and routes and deply heroku and create scema and soo on

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 require('env2')('./config.env');
 let dbURL = '';
-if(!process.env.URL_test_CITYS && !process.env.URL_H_CITYS && !process.env.URL_CITYS) throw new Error('NO DATABASE URL!!!');
 if(process.env.NODE_ENV === 'test') dbURL = process.env.URL_test_CITYS;
 else if(process.env.NODE_ENV === 'development') dbURL = process.env.URL_CITYS; 
 else dbURL = process.env.CITYS_H_URL;
+if(!dbURL) throw new Error('NO DATABASE URL!!!');
 
 
 module.exports = new Pool({
